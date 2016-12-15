70b25aafe322df62e1238fa4e3ac25b4876f6d78
Michaël Arnauts <michael.arnauts@gmail.com>
2016-12-15 23:37:18 +0100

    Create python classes to handle the communication and messages.

--- test_connect.py
+++ test_connect.py
@@ -1,120 +1,61 @@
 #!/usr/bin/python3
-import socket
-from zehnder_pb2 import *
-import struct
+import comfoconnect
+import argparse
+import time
 
-ZEHNDER_IP =    "192.168.1.213"
-ZEHNDER_PORT =  56747
-ZEHNDER_UUID = "0000000000251010800170b3d54264b4"
-MY_UUID =      "af154804169043898d2da77148f886be"
+parser = argparse.ArgumentParser()
+parser.add_argument("ip", help="ip address of the bridge")
+parser.add_argument("uuid", help="uuid of the bridge")
+parser.add_argument("my_uuid", help="uuid of the local machine")
 
-def send_message(sock, cmd, msg):
-    """ Send a serialized message (protobuf Message interface)
-        to a socket, prepended by a header (length, src, dst).
-    """
-    cmd_buf = cmd.SerializeToString()
-    msg_buf = msg.SerializeToString()
-    header = struct.pack('>L16s16sH', 32 + 2 + len(cmd_buf) + len(msg_buf), bytearray.fromhex(MY_UUID), bytearray.fromhex(ZEHNDER_UUID), len(cmd_buf))
+args = parser.parse_args()
 
-    print('tx:' + header.hex() + cmd_buf.hex() + msg_buf.hex())
-    sock.sendall(header + cmd_buf + msg_buf)
-
-def get_message(sock, msgtype):
-    """ Read a message from a socket. msgtype is a subclass of protobuf Message.
-    """
-
-    # Read full packet
-    msg_len_buf = sock.recv(4)
-    msg_len = struct.unpack('>L', msg_len_buf)[0]
-    msg_buf = sock.recv(msg_len)
-
-    # Extract headers
-    src_buf = msg_buf[:16]
-    dst_buf = msg_buf[16:32]
-    cmd_len_buf = msg_buf[32:34]
-    cmd_len = struct.unpack('>H', cmd_len_buf)[0]
-    cmd_buf = msg_buf[34:34+cmd_len]
-    msg_buf = msg_buf[34+cmd_len:]
-
-    print('rx:')
-    print(msg_len_buf.hex())
-    print(src_buf.hex())
-    print(dst_buf.hex())
-    print(cmd_len_buf.hex())
-    print(cmd_buf.hex())
-    print(msg_buf.hex())
+def callback(cmd, msg):
+    pass
 
 
-    cmd = msgtype()
-    cmd.ParseFromString(cmd_buf)
+bridge = comfoconnect.Bridge(args.ip, args.uuid, callback)
+bridge.connect(args.my_uuid)
 
-    msg_type = cmd.type
+# Register with the bridge
+try:
+    bridge.RegisterApp('Computer', 0)
+    print("Successfully registered")
+except:
+    print('Could not register. Invalid PIN!')
+    exit(1)
 
+# Start session
+bridge.StartSession()
 
-    print(msg_type)
+# Read a notification message
+cmd, msg = bridge._read_message()
 
+# Read a notification message
+cmd, msg = bridge._read_message()
 
-    exit()
-
-    src_buf = socket_read_n(sock, 16)
-    dst_buf = socket_read_n(sock, 16)
-    cmd_len_buf = socket_read_n(sock, 2)
-
-
-    msg = msgtype()
-    msg.ParseFromString(msg_buf)
-    return msg
-
-def socket_read_n(sock, n):
-    """ Read exactly n bytes from the socket.
-        Raise RuntimeError if the connection closed before
-        n bytes were read.
-    """
-    buf = ''
-    while n > 0:
-        data = sock.recv(n)
-        if data == '':
-            raise RuntimeError('unexpected connection close')
-        buf += data
-        n -= len(data)
-    return buf
-
-# Setup socket and connect
-tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsocket.connect((ZEHNDER_IP, ZEHNDER_PORT))
-
-
-# Register
-cmd = GatewayOperation()
-cmd.type = GatewayOperation.OperationType.Value('RegisterAppRequestType')
-cmd.reference = 2
-
-msg = RegisterAppRequest()
-msg.uuid = bytes.fromhex(MY_UUID)
-msg.pin = 0
-msg.devicename = 'Computer'
-send_message(tcpsocket, cmd, msg)
-
-# Register reply
-rpl = get_message(tcpsocket, GatewayOperation)
-print(rpl)
-
-tcpsocket.close()
+# Close session
+bridge.CloseSession()
 
 
 
-# Login
-msg = GatewayOperation()
-msg.type = GatewayOperation.OperationType.Value('StartSessionRequestType')
-msg.reference = 1
-send_message(tcpsocket, 4, msg)
 
-# Login reply
-rpl = get_message(tcpsocket, GatewayOperation)
-print(rpl)
 
-tcpsocket.close()
-
+#
+#
+#
+# # Login
+# msg = GatewayOperation()
+# msg.type = GatewayOperation.OperationType.Value('StartSessionRequestType')
+# msg.reference = 1
+# send_message(tcpsocket, 4, msg)
+#
+# # Login reply
+# rpl = get_message(tcpsocket, GatewayOperation)
+# print(rpl)
+#
+# tcpsocket.close()
+#
 
 
 # Logout

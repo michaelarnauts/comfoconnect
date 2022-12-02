254570084d8b1c78798cbf8e6f7424b6a92f846d
Michaël Arnauts <michael.arnauts@gmail.com>
2022-12-02 17:11:42 +0100

    Release 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='pycomfoconnect',
-    version='0.5',
+    version='0.5.1',
     license='MIT',
     url='https://github.com/michaelarnauts/comfoconnect',
     author='Michaël Arnauts',
